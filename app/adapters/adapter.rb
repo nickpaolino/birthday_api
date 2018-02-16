1c09b3546220ff1f003b873bae32e89b5ac4c846
Nick Paolino <github email address>
2018-02-16 15:11:09 -0500

    Refactored getter methods in IMDB class as attr accessors

--- app/adapters/adapter.rb
+++ app/adapters/adapter.rb
@@ -1,16 +1,11 @@
 module Adapter
   class IMDB
+    attr_accessor :date, :num_of_results
+
     def initialize(date, num_of_results = nil)
       @date = date
       @num_of_results = num_of_results
     end
 
-    def date
-      @date
-    end
-
-    def num_of_results
-      @num_of_results
-    end
   end
 end
