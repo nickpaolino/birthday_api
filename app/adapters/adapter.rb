716e76afd7442c114fd56657c9bfaaf10bae916e
Nick Paolino <github email address>
2018-02-16 15:04:28 -0500

    Created Adapter Module with IMDB class for scraping

---
+++ app/adapters/adapter.rb
@@ -0,0 +1,11 @@
+module Adapter
+  class IMDB
+    def initialize(date)
+      @date = date
+    end
+
+    def date
+      @date
+    end
+  end
+end
--- app/controllers/birthday_controller.rb
+++ app/controllers/birthday_controller.rb
@@ -2,6 +2,9 @@ class BirthdayController < ApplicationController
   def show
     date = params["q"]
     num_of_results = params["results"]
-    render json: {birthday: date, results: num_of_results}
+
+    adapter = Adapter::IMDB.new(date)
+
+    render json: {birthday: adapter.date, results: num_of_results}
   end
 end
